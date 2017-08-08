65cd256a8dfdef74974cb6d1722a0bed5b668916
DESKTOP-V9NOAJD\Fakhri Rido Muhammad <irhkafodir@gmail.com>
2017-08-08 21:54:03 +0700

    -Add master -Game is working

---
+++ Assets/Script/CoreGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CoreGame : MonoBehaviour
+{
+    public GameObject ChangingText;
+    private Text text;
+    
+
+    public static int computerTrials;
+    public int guess;
+    private int maxGuess;
+    int min;
+    int max;
+
+    private void Start()
+    {
+        maxGuess = Random.Range(12,15);
+        text = ChangingText.GetComponent<Text>();
+
+        guess = Random.Range(1, 1000);
+        min = 1;
+        max = 1000;
+        computerTrials = 1;
+        text.text = "Is your number equals " + guess + " ?";
+    }
+
+    public void ProcessGuess()
+    {
+        guess = Random.Range(min, max);
+        computerTrials += 1;
+        if (computerTrials == maxGuess)
+        {
+            Application.LoadLevel("Win");
+        }
+        text.text = "Is your number equals " + guess + " ?";
+    }
+
+    public void Higher()
+    {
+        min = guess;
+        ProcessGuess();
+    }
+
+    public void Lower()
+    {
+        max = guess;
+        ProcessGuess();
+    }
+
+
+}
---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+
+    public void quitRequest()
+    {
+        Application.Quit();
+    }
+}
